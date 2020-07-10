d58bcaaefab4a950724e374240c8a1d56cb666ee
clueking1 <wcoan300@gmail.com>
2020-07-10 09:04:07 -0400

    added onChange and onClick and function to handle API route for newName

--- src/Components/InputNames/index.js
+++ src/Components/InputNames/index.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
+//API Import
+import API from '../../Utils/API'
+
 function InputNames() {
+    const [name, setName] = useState('')
+
+    function submit() {
+        API.newName({
+            name : name
+        })
+    }
+
     return (
         <div className="inputDiv">
             <input
                 className="nameIn"
+                onChange={e => setName(e.target.value)}
             />
             <br />
             <button 
                 type="submit"
+                onClick={() => submit()}
             >Submit Name!</button>
         </div>
         
