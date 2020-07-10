4f7e86cf0d8a9001b090cfec964d4bc62689abe4
clueking1 <wcoan300@gmail.com>
2020-07-10 08:58:19 -0400

    add alphanumerical string to url on page load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+
+//Pages' imports
 import MainPage from './Pages/MainPage'
+
+//npm imports
+import randomstring from 'randomstring'
 
 
 function App() {
+    const [string, setString] = useState('')
+    
+    useEffect(() => {
+        let random = randomstring.generate({
+            length: 12,
+            charset: 'alphanumeric'
+          });
+        setString(random)
+    },[])
     return (
-        <div className="appWrap">
-            <MainPage />
-        </div>
+        <Router>
+            <div className="appWrap">
+                <Switch>
+                    <MainPage path='/' component={string}/>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
