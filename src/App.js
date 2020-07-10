8e94cd9b65c0fdc73f5f0d82df00051bed5fc0a5
clueking1 <wcoan300@gmail.com>
2020-07-10 07:41:40 -0400

    added a simple subtle background with gradient pattern and routed it to display on window

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainPage from './Pages/MainPage'
+
+
+function App() {
+    return (
+        <div className="appWrap">
+            <MainPage />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Pages/MainPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+
+function MainPage() {
+    return (
+        <div className="mainBackground">
+            <div className="mainColor">
+
+            </div>
+        </div>
+    )
+}
+
+export default MainPage
